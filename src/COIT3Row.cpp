bbdfecfe5b94d895608bb5bd4481c1266135f14d
Brian Kloppenborg <bkloppenborg@gmail.com>
2013-06-26 10:10:09 +0200

    Converted the (u3,v3) point to conform to the OIFITS specification exactly. Closes #4.

--- src/COIT3Row.cpp
+++ src/COIT3Row.cpp
@@ -23,8 +23,8 @@ COIT3Row::COIT3Row(OITargetPtr target, OIArrayPtr array, OIWavelengthPtr wavelen
 
 	// Construct the uv31 UV point:
 	OIUVPtr uv31 = OIUVPtr( new COIUV );
-	uv31->u = -(uv12->u + uv23->u);
-	uv31->v = -(uv12->v + uv23->v);
+	uv31->u = uv12->u + uv23->u;
+	uv31->v = uv12->v + uv23->v;
 	mUV.push_back(uv31);
 }
 
